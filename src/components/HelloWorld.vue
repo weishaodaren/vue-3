fd40d871c2014dea72e0fbe5b61758d0817a1dce
weishaodaren <zhaojiawei@realwaychina.com>
2020-07-11 23:36:52 +0800

    ref 在模板中、reactive中可以不写.value自动解套 ref后期赋值会被替代

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,24 +1,63 @@
 <template>
-  <div>{{ attrs_ }}{{ attrs.name }}</div>
+  <div>
+    {{ attrs_ }}{{ reactiveAttrs.name }}
+    <h1 @click="handleClick">click me</h1>
+    {{ count }}
+    <br />
+    {{ state }}
+  </div>
 </template>
 
 <script>
-import { watchEffect, ref, reactive } from "vue";
+import { watchEffect, ref, reactive, computed } from "vue";
 
 export default {
   props: {
     msg: String
   },
-  setup(props, context) {
+  setup(props, { attrs, slots, emit }) {
+    // context {attrs、slots、emit}
     watchEffect(() => {
       console.log(`msg is ${props.msg}`);
-      console.log(context.attrs);
+      console.log(attrs);
     });
-    const attrs_ = ref(context.attrs);
-    const attrs = reactive(context.attrs);
+    const attrs_ = ref(attrs);
+    const reactiveAttrs = reactive(attrs);
+    const handleClick = () => {
+      console.log(slots);
+      console.log(emit);
+      console.log(attrs);
+    };
+    const count = ref(1);
+    const state = reactive({ count });
+    const otherCount = ref(999);
+    const one = count;
+    state.count = otherCount.value; // ref 被代替
+    console.log(count.value, one.value);
+
+    const count_0 = ref(1);
+    const plusOne = computed(() => count_0.value + 1);
+    count_0.value = 10;
+    console.log(plusOne.value);
+
+    const count_1 = ref(2);
+    const plusOne_1 = computed({
+      get: () => count_1.value + 1,
+      set: val => {
+        count_1.value = val * 10 + 857;
+      }
+    });
+    plusOne_1.value = 10.02;
+    count_1.value = -100;
+    plusOne_1.value = -1000;
+    console.log(plusOne_1.value);
+
     return {
       attrs_,
-      attrs
+      reactiveAttrs,
+      handleClick,
+      count,
+      state
     };
   }
 };
